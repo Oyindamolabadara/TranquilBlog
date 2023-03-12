5061d0edecfdaa95652846a24699278db0efebb2
Ganiat Badara <ganiyatbadara@gmail.com>
2023-03-12 21:42:39 +0000

    Changed default profile image. Edited blog detail update button and get help now button.

--- users/models.py
+++ users/models.py
@@ -6,8 +6,7 @@ from cloudinary.models import CloudinaryField
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-    featured_image = CloudinaryField('image', default='profile_default.jpeg')
+    featured_image = CloudinaryField('image')
 
     def __str__(self):
         return f'{self.user.username} Profile'
